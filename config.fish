15be4eeb4ad6f5277c0e24aeff24a77fb579fa8a
Gandalf- <austin.voecks@gmail.com>
2017-08-20 21:17:40 -0700

    Add file content search to f

--- config.fish
+++ config.fish
@@ -127,7 +127,7 @@ if test "$wiki_loc"
 end
 
 function f
-  # f [zlct] [target]
+  # f [azlct] [target]
   #
   # f       -> cd ~
   # f file  -> vim file
@@ -136,26 +136,33 @@ function f
 
   if test -z "$argv"; cd; return; end
   
-  set has_flag
   set files
   set fuzzy
+  set grepper
+  set has_flag
   set locate
   set move
   set notimeout
+  set vim_opt
 
   # check for fuzzy, locate, move flags
-  if not string match -q -r '[^zlct-]' $argv[1]
+  if not string match -q -r '[^azlct-]' $argv[1]
     if string match -q -r '.*z.*' $argv[1]; set fuzzy     "yes"; end
     if string match -q -r '.*l.*' $argv[1]; set locate    "yes"; end
     if string match -q -r '.*c.*' $argv[1]; set move      "yes"; end
     if string match -q -r '.*t.*' $argv[1]; set notimeout "yes"; end
-    if string match -q '-' $argv[1]; cd -; return; end
+    if string match -q -r '.*t.*' $argv[1]; set notimeout "yes"; end
+    if string match -q -r '.*a.*' $argv[1]; set grepper   "yes"; end
+    if string match -q    '-'     $argv[1]; cd -; return; end
     set --erase argv[1]
     set has_flag "yes"
   end
 
   for arg in $argv
-    if test -f "$arg"                                       # file
+    if test (string sub -l 1 "$arg") = '+'                  # vim option
+      set vim_opt "$arg"
+
+    else if test -f "$arg"                                  # file
       set files $arg $files
 
     else if test -d "$arg"                                  # directory
@@ -167,23 +174,32 @@ function f
     else                                                    # search
       set search
 
-      if test "$fuzzy"
-        if test "$notimeout"
-          set search (find . -name '*'"$arg"'*')
-        else
-          set search (timeout 1 find . -name '*'"$arg"'*')
-        end
-      else
-        if test "$notimeout"
-          set search (find . -name "$arg")
-        else
-          set search (timeout 1 find . -name "$arg")
-        end
+      if test "$grepper"                                      # contents
+        set cmd "ag"
+        set flags "-l"
+
+        test "$fuzzy";     and set flags "$flags" -i
+        test "$notimeout"; and set cmd   "timeout 1 $cmd"
+
+        set search (eval $cmd $flags $arg)
+        test "$search"; and not test "$fuzzy"; and set vim_opt "+/$arg"
+
+      else                                                    # name
+        set cmd "find . "
+        set flags "-name $arg -not -path './.*'"
+
+        test "$fuzzy";     and set flags "-iname '*'$arg'*' -not -path './.*'"
+        test "$notimeout"; and set cmd "timeout 1 $cmd"
+
+        set search (eval $cmd $flags)
       end
 
       if test "$search"
-        for new_file in $search; set files "$new_file" $files; end
+        for new_file in $search
+          set files "$new_file" $files
+        end
       end
+
     end
   end
   
@@ -197,7 +213,7 @@ function f
       ls --color=auto
 
     else
-      vim -p $files
+      vim -p $files $vim_opt
     end
   end
 end
