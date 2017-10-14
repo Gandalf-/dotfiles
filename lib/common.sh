c861236ffb40d5ba9a67b5af0c8e2e5a049a8afd
Gandalf- <austin.voecks@gmail.com>
2017-10-14 16:10:55 -0700

    Refactor g, add undotree to Vim

--- lib/common.sh
+++ lib/common.sh
@@ -58,21 +58,28 @@ common::do() {
   # print what we're about to do, then do it
   local green="\033[01;32m" normal="\033[00m"
 
-  printf "%b%s%b\n" "$green" "$*" "$normal"
+  if ! (( "$AUTO" )); then
+    printf "%b%s%b\n" "$green" "$*" "$normal"
+  fi
+
+  if (( "$CONFIRM" )); then
+    read -r reply
+    [[ "$reply" =~ [Nn] ]] && exit 1
+  fi
 
   if (( "$ECHO" )); then
     true
 
   elif (( "$QUIET" )); then
-    eval "$@" >/dev/null \
+    eval "${@/ \"\"/}" >/dev/null \
       || common::color_error "error running \"$*\""
 
   elif (( "$SILENT" )); then
-    eval "$@" >/dev/null 2>/dev/null \
+    eval "${@/ \"\"/}" >/dev/null 2>/dev/null \
       || common::color_error "error running \"$*\""
 
   else
-    eval "$@" \
+    eval "${@/ \"\"/}" \
       || common::color_error "error running \"$*\""
   fi
 }
@@ -83,23 +90,12 @@ common::sudo() {
   common::do "sudo" "$@"
 }
 
-confirm() {
+common::confirm() {
   local green="\033[01;32m" normal="\033[00m"
 
-  if [[ "$1" != 0 ]]; then
-    shift
-    printf "%b%s%b " "$green" "$@" "$normal"
+  printf "%b%s%b " "$green" "$@" "$normal"
+
+  if ! (( "$CONFIRM" )); then
     read -r reply; [[ "$reply" =~ [Nn] ]] && exit 1
-    return 0
-
-  else
-    shift
-    printf "%b%s%b\n" "$green" "$@" "$normal"
-    return 0
   fi
-}
-
-_confirm() {
-  val=$1; shift
-  confirm "$val" "${@/ \"\"/}" && eval "${@/ \"\"/}"
 }
