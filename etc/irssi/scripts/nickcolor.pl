bfabcb480b880f8c69825710db9643b6bc02d4b0
Gandalf- <austin.voecks@gmail.com>
2017-11-19 07:55:56 -0800

    Minify perl scripts

--- etc/irssi/scripts/nickcolor.pl
+++ etc/irssi/scripts/nickcolor.pl
@@ -11,15 +11,8 @@ $VERSION = "2";
     url   => "http://irssi.org/",
     changed => "Sun 15 Jun 19:10:44 BST 2014",
 );
-
-# Settings:
-#   nickcolor_colors: List of color codes to use.
-#   e.g. /set nickcolor_colors 2 3 4 5 6 7 9 10 11 12 13
-#   (avoid 8, as used for hilights in the default theme).
-
 my %saved_colors;
 my %session_colors = {};
-
 sub load_colors {
   open my $color_fh, "<", "$ENV{HOME}/.irssi/saved_colors";
   while (<$color_fh>) {
@@ -28,80 +21,52 @@ sub load_colors {
     $saved_colors{$nick} = $color;
   }
 }
-
 sub save_colors {
   open COLORS, ">", "$ENV{HOME}/.irssi/saved_colors";
-
   foreach my $nick (keys %saved_colors) {
     print COLORS "$nick:$saved_colors{$nick}\n";
   }
-
   close COLORS;
 }
-
-# If someone we've colored (either through the saved colors, or the hash
-# function) changes their nick, we'd like to keep the same color associated
-# with them (but only in the session_colors, ie a temporary mapping).
-
 sub sig_nick {
   my ($server, $newnick, $nick, $address) = @_;
   my $color;
-
   $newnick = substr ($newnick, 1) if ($newnick =~ /^:/);
-
   if ($color = $saved_colors{$nick}) {
     $session_colors{$newnick} = $color;
   } elsif ($color = $session_colors{$nick}) {
     $session_colors{$newnick} = $color;
   }
 }
-
-# This gave reasonable distribution values when run across
-# /usr/share/dict/words
-
 sub simple_hash {
   my ($string) = @_;
   chomp $string;
   my @chars = split //, $string;
   my $counter;
-
   foreach my $char (@chars) {
     $counter += ord $char;
   }
-
   my @colors = split / /, Irssi::settings_get_str('nickcolor_colors');
   $counter = $colors[$counter % @colors];
-
   return $counter;
 }
-
 sub sig_public {
   my ($server, $msg, $nick, $address, $target) = @_;
-
-  # Has the user assigned this nick a color?
   my $color = $saved_colors{$nick};
-
-  # Have -we- already assigned this nick a color?
   if (!$color) {
     $color = $session_colors{$nick};
   }
-
-  # Let's assign this nick a color
   if (!$color) {
     $color = simple_hash $nick;
     $session_colors{$nick} = $color;
   }
-
   $color = sprintf "\003%02d", $color;
   $server->command('/^format pubmsg {pubmsgnick $2 {pubnick '.chr(3).$color.'$[-10]0}}$1');
 }
-
 sub cmd_color {
   my ($data, $server, $witem) = @_;
   my ($op, $nick, $color) = split " ", $data;
-
   $op = lc $op;
-
   if (!$op) {
     Irssi::print ("No operation given (save/set/clear/list/preview)");
   } elsif ($op eq "save") {
@@ -135,11 +100,8 @@ sub cmd_color {
     }
   }
 }
-
 load_colors;
-
 Irssi::settings_add_str('misc', 'nickcolor_colors', '2 3 4 5 6 7 9 10 11 12 13');
 Irssi::command_bind('color', 'cmd_color');
-
 Irssi::signal_add('message public', 'sig_public');
 Irssi::signal_add('event nick', 'sig_nick');
