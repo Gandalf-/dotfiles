9131b7ee56ac86c2d643ba53ee9f1ad2f74ae213
Gandalf- <austin.voecks@gmail.com>
2018-02-15 10:40:12 -0800

    better wizard http server

--- lib/wizard/wizard.sh
+++ lib/wizard/wizard.sh
@@ -228,7 +228,40 @@ wizard_show_largest-packages() {
 
 wizard_start_http-server() {
 
-  python -m SimpleHTTPServer
+  common::optional-help "$1" "(arguments)
+
+  start an http server
+  "
+
+python3 -c "
+from http.server import SimpleHTTPRequestHandler, test
+import argparse
+
+class InlineHandler(SimpleHTTPRequestHandler):
+
+    def end_headers(self):
+        mimetype = self.guess_type(self.path)
+        is_file = not self.path.endswith('/')
+        print(mimetype, is_file)
+
+        # This part adds extra headers for some file types.
+        if is_file and mimetype in ['text/plain', 'application/octet-stream']:
+            self.send_header('Content-Type', 'text/plain')
+            self.send_header('Content-Disposition', 'inline')
+        super().end_headers()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--bind', '-b', default='', metavar='ADDRESS',
+                        help='Specify alternate bind address '
+                             '[default: all interfaces]')
+    parser.add_argument('port', action='store',
+                        default=8000, type=int,
+                        nargs='?',
+                        help='Specify alternate port [default: 8000]')
+    args = parser.parse_args()
+    test(InlineHandler, port=args.port, bind=args.bind)
+"
   return 0
 }
 
