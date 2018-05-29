f69dafa58a6042e9579c3bae3ede54cacdc35984
Gandalf- <austin.voecks@gmail.com>
2018-05-28 18:04:24 -0700

    devbot error backoff, clean up

--- lib/devbot.sh
+++ lib/devbot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+# shellcheck disable=SC2155
+
 # devbot library
 #
 #   daemon to handle background tasks automatically based on a schedule
-#
-#   tasks are added to a schedule file, that's read every 5 seconds.
 #
 #   when a tasks time is reached, the command is run and it's added back to the
 #   schedule with an updated time (current time + interval)
@@ -12,38 +12,43 @@
 #   this approach makes devbot's schedule persist between runs, allowing very
 #   infrequent tasks to be scheduled with confidence
 
-devbot::task:handle() {
+devbot::log() {
+  printf '%s - %s\n' "$(date +'%x %X')" "$*"
+}
+
+devbot::task() {
 
   # event -> none
   #
   # check the time field of the input, if it's passed then run the command.
   # otherwise we add it back to schedule unchanged
 
+
   common::debug "task:handle $*"
 
   local event="$1"
-  local interval when action require
+  local now="$(date '+%s')"
+  local when="$(d devbot data "$event" when)"
 
-  when="$(d devbot data "$event" when)"
-
-  if (( when < $(date '+%s') )); then
+  if (( when < now )); then
 
     # check for prerequisites
-    require="$(d devbot events "$event" require)"
+    local req_name="$(d devbot events "$event" require)"
 
-    if [[ $require ]]; then
+    if [[ $req_name ]]; then
 
       # grab the action to run out of the table
-      require="$(d devbot requirements "$require")"
+      local req_action="$(d devbot requirements "$req_name")"
 
-      if ! eval "$require"; then
-        echo "$event requirement \"$require\" not met"
+      if ! eval "$req_action"; then
+        devbot::log \
+          "$event requirement \"$req_name\" ($req_action) not met"
         return
       fi
     fi
 
-    # run the event, update time
-    interval="$(d devbot events "$event" interval)"
+    # update when to run next
+    local interval="$(d devbot events "$event" interval)"
     case $interval in
       daily)
         interval=86400
@@ -55,10 +60,15 @@ devbot::task:handle() {
         interval=604800
         ;;
     esac
-    action="$(d devbot events "$event" action)"
-    [[ $action ]] || { echo "task:handle error: no action"; return; }
+    d devbot data "$event" when = $(( interval + now ))
 
-    d devbot data "$event" when = $(( interval + $(date '+%s') ))
+    # run the action
+    local action="$(d devbot events "$event" action)"
+    [[ $action ]] || {
+      devbot::log "task:handle error: no action"
+      return
+    }
+
     devbot::eval "$event" "$action"
   fi
 }
@@ -68,16 +78,32 @@ devbot::eval() {
 
   # string, string ... -> none
   #
-  # evaluate the given shell code by throwing in into the wind
+  # evaluate the given shell code, but don't wait for it to finish
 
   local name="$1"; shift
   common::debug "eval: $*"
 
   {
-    local begin end
-    begin="$(date '+%s')"
-    bash -c "set -e; $*" || echo "error while running \"$*\""
-    end="$(date '+%s')"
+    local begin="$(date '+%s')"
+
+    bash -c "set -e; $*" || {
+
+      # the command failed
+      # increment number of errors
+      local errors="$(d devbot data "$name" errors)"
+      [[ $errors ]] || errors="0"
+      (( errors += 1))
+      d devbot data "$name" errors = "$errors"
+
+      # set the backoff
+      local backoff="$(( 10 * errors ))"
+      local when="$(d devbot data "$name" when)"
+      d devbot data "$name" when = "$(( when + backoff ))"
+
+      # log it
+      devbot::log "error while running \"$name\" ($*), backing off $backoff seconds"
+    }
+    local end="$(date '+%s')"
 
     d devbot data "$name" duration = "$(( end - begin ))"
   } &
@@ -93,12 +119,11 @@ devbot::main() {
 
   mkdir -p ~/.devbot
 
-  echo "starting devbot"
+  devbot::log "starting devbot"
   while :; do
 
     while read -r event; do
-      devbot::task:handle "$event"
-
+      devbot::task "$event"
     done < <(d devbot events --keys)
 
     sleep 5
