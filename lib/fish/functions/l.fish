3c75a3ea368b38da1af8dbb24c80fb1f1bd43a46
avoecks <austin.voecks@isilon.com>
2019-06-14 10:26:51 -0700

    sync up

--- lib/fish/functions/l.fish
+++ lib/fish/functions/l.fish
@@ -1,15 +1,13 @@
 # vim: set syntax=bash
 
 function l
-  if ls --version >/dev/null 2>/dev/null;
-    ls --color=auto "$argv"
 
-  else
-    if string length -- "$argv" >/dev/null
-      ls -G "$argv"
-    else
-      ls -G
-    end
+  # bsd
+  set -- color "-G"
 
-  end
+  # gnu
+  ls --version >/dev/null 2>/dev/null;
+    and set -- color "--color=auto"
+
+  ls $color $argv
 end
